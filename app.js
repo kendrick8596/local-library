46219a5cdbad946f33a1bd43e00ec8bb79483565
Kendrick <kendrick8596@gmail.com>
2024-06-19 01:33:50 +0000

    Created Author Schema

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// setup mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = 'mongodb+srv://khump:nZtvsXSoeNAznYfm@librarycluster.vqrtyjj.mongodb.net/?retryWrites=true&w=majority&appName=LibraryCluster';
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
---
+++ models/author.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema(
+    {
+    first_name: { 
+        type: String,
+        required: true,
+        maxLength: 100,
+    },
+    family_name: {
+        type: String,
+        required: true,
+        maxLength: 100,
+    },
+    date_of_birth: {
+        type: Date,
+    },
+    date_of_death: {
+        type: Date,
+    },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual('name').get(function() {
+    // avoid errors where author does not have a family or first name
+    // handle the exception by returning an empty string for that case
+    let fullname = '';
+    if (this.first_name && this.family_name) {
+        fullname = `${this.family_name}, ${this.first_name}`;
+    }
+
+    return fullname;
+});
+
+// Virtual for authors URL
+AuthorSchema.virtual('url').get(function () {
+    // do not use arrow function as we will need the .this object
+    return `/catalog/author/${this._id}`;
+});
+
+//export the model
+module.exports = mongoose.model('Author', AuthorSchema);
