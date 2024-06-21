6ef1e08ed8b67541fc164c788cfa779d7027279a
Kendrick <kendrick8596@gmail.com>
2024-06-21 00:05:16 +0000

    Created Author Page with Luxon

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -38,6 +39,16 @@ AuthorSchema.virtual('name').get(function() {
 AuthorSchema.virtual('url').get(function () {
     // do not use arrow function as we will need the .this object
     return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+    return this.date_of_birth ?
+            DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+    return this.date_of_death ?
+            DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 //export the model
