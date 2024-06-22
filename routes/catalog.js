3c2f103606d2786c2d0626f0b5dcd4bbcd940aee
Kendrick <kendrick8596@gmail.com>
2024-06-22 00:27:09 +0000

    Created Genre Details Page

--- routes/catalog.js
+++ routes/catalog.js
@@ -80,10 +80,10 @@ router.post('/genre/:id/delete', genre_controller.genre_delete_post);
 router.get('/genre/:id/update', genre_controller.genre_update_get);
 
 // POST request to update Genre.
-router.get('genre/:id/update', genre_controller.genre_update_post);
+router.get('/genre/:id/update', genre_controller.genre_update_post);
 
 // GET request for one Genre.
-router.get('genre/:id', genre_controller.genre_detail);
+router.get('/genre/:id', genre_controller.genre_detail);
 
 // GET request for list of all Genre.
 router.get('/genres', genre_controller.genre_list);
